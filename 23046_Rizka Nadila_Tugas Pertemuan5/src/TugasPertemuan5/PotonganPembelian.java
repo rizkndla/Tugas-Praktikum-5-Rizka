faf797da61bd71b52ddd5a4fbed7f5396392dee9
Rizka Nadila <148744758+rizkndla@users.noreply.github.com>
2024-10-02 13:53:40 +0700

    PotonganPembelian.java

--- 23046_Rizka Nadila_Tugas Pertemuan5/src/TugasPertemuan5/PotonganPembelian.java
+++ 23046_Rizka Nadila_Tugas Pertemuan5/src/TugasPertemuan5/PotonganPembelian.java
@@ -7,22 +7,18 @@ package TugasPertemuan5;
         public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // Input total pembelian
         System.out.print("Masukkan total pembelian: ");
         double totalPembelian = input.nextDouble();
 
-        // Menentukan potongan
         double potongan;
         if (totalPembelian >= 50000) {
-            potongan = totalPembelian * 0.20;  // 20% potongan
+            potongan = totalPembelian * 0.20;  
         } else {
-            potongan = totalPembelian * 0.05;  // 5% potongan
+            potongan = totalPembelian * 0.05; 
         }
 
-        // Hitung jumlah yang harus dibayar
         double jumlahYangDibayar = totalPembelian - potongan;
 
-        // Output hasil
         System.out.println("Total pembelian: Rp " + totalPembelian);
         System.out.println("Besarnya potongan: Rp " + potongan);
         System.out.println("Jumlah yang harus dibayar: Rp " + jumlahYangDibayar);
